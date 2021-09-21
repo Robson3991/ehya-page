cdbfbe83402e4a5a5a7b273ed78d2a37c599a476
Robert Grembowicz <robert.grembowicz@gmail.com>
2021-09-21 19:21:53 +0200

    move header to organism folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Header from 'components/molecules/Header/Header';
+import Header from 'components/organisms/Header/Header';
 
 export default function Home() {
   return (
